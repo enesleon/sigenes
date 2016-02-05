e5ef509589c7a51043c3becc58f28479ab0a4ccb
JorgeNaranjo90 <jorge020890@gmail.com>
2016-02-05 01:42:22 -0600

    [ADD] Added main templates form forms

--- sigenes/resources/views/home.blade.php
+++ sigenes/resources/views/home.blade.php
@@ -1,17 +1,23 @@
-@extends('layouts.app')
+@extends('layouts.generals.main_template')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
+@section('body_page')
+    <div class="col-lg-offset-3 col-md-offset-2 col-lg-6 col-md-8 col-sm-12 col-xs-12 text-center">
+        <div class="column">
+            <div class="header">
+                <h1 class="ui header">
+                    Hello {{ Auth::user()->name }}
+                </h1>
+            </div>
+            <div class="image content">
+                <div class="ui medium image centered">
+                    <img src="http://placehold.it/300x300">
+                </div>
+                <div class="description centered">
+                    <div class="ui header">We've auto-chosen a profile image for you.</div>
+                    <p>We've grabbed the following image from the <a href="https://www.gravatar.com" target="_blank">gravatar</a> image associated with your registered e-mail address.</p>
+                    <p>Is it okay to use this photo?</p>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
